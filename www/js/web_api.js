7b13996613ce2d00f5ba01af9b8ce3ffb6682d85
Aaron Rosen <aaronorosen@gmail.com>
2020-10-29 14:43:49 -0700

    add integration for sms client

--- www/js/web_api.js
+++ www/js/web_api.js
@@ -8,6 +8,8 @@ function get_users() {
 }
 
 
+
+
 function get_user_list(callback) {
 
     var settings = {
@@ -27,3 +29,56 @@ function get_user_list(callback) {
     })
 }
 
+
+function send_user_sms(to_number, msg, callback) {
+    var form = new FormData();
+    form.append("to_number", to_number);
+    form.append("msg", msg);
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": SERVER + "voip/api_voip/send_sms",
+        "method": "POST",
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": form,
+        "headers": {
+            "Authorization": localStorage.getItem("token"),
+        },
+
+    }
+    $.ajax(settings).done(function (response) {
+        // change screen for code collecton
+        console.log(response)
+    }).fail(function (err) {
+      //alert("ERROR")
+    });
+}
+
+function get_sms_to_number(to_number, callback) {
+    var form = new FormData();
+    form.append("to_number", to_number);
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "url": SERVER + "voip/api_voip/list_sms",
+        "method": "POST",
+    }
+    $.ajax(settings).done(function (response) {
+        response = JSON.parse(response)
+        // XXX Santosh here is what msg look like to populate
+        // SMS chat interface.
+        for(var msg of response.messages) {
+            console.log(msg);
+        }
+        callback(response.messages)
+    }).fail(function (err) {
+        alert("ERROR")
+    })
+}
