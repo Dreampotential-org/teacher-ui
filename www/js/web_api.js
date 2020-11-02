cedecf241f0b013eb089dbf5eead25e426c4a8a9
Aaron Rosen <aaronorosen@gmail.com>
2020-11-02 13:56:34 -0800

    add api integration point for calls: list_inbound_calls()

--- www/js/web_api.js
+++ www/js/web_api.js
@@ -7,6 +7,25 @@ function get_users() {
     });
 }
 
+function list_inbound_calls(callback) {
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": SERVER + "admin_backend/api_admin/list_calls",
+        "method": "GET",
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+
+    }
+    $.ajax(settings).done(function (response) {
+        callback(JSON.parse(response))
+    }).fail(function (err) {
+      alert("ERROR")
+    })
+}
+
 
 
 
