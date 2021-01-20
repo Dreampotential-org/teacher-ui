02d39bed3da5334fc363630e4985df04b6cfd906
bhavinsen <senbhavin@gmail.com>
2021-01-20 17:36:11 +0530

    add student API connect

--- www/js/class_list.js
+++ www/js/class_list.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
 $("#left-sidebar").load("sidebar.html");
 $("#page-header").load("header.html");
+
 //                        $("#page-container").load('dashboard.html');
 });
 $("#body-row .collapse").collapse("hide");
@@ -33,41 +34,41 @@ $("#body-row .collapse").collapse("hide");
         // Collapse/Expand icon
         //$('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
       }
-      var system_users = [
-        {
-          class_id: "CLS1",
-          class_name: "Class A",
-          // student_name: "John",
-        },
-        {
-          class_id: "CLS2",
-          class_name: "Class B",
-          // student_name: "Moe",
-        },
-        {
-          class_id: "CLS3",
-          class_name: "Class C",
-          // student_name: "Dooley",
-        },
-      ];
+    //   var system_users = [
+    //     {
+    //       class_id: "CLS1",
+    //       class_name: "Class A",
+    //       // student_name: "John",
+    //     },
+    //     {
+    //       class_id: "CLS2",
+    //       class_name: "Class B",
+    //       // student_name: "Moe",
+    //     },
+    //     {
+    //       class_id: "CLS3",
+    //       class_name: "Class C",
+    //       // student_name: "Dooley",
+    //     },
+    //   ];
 
-      var system_students = [
-        {
-          student_name: "John",
-          student_email: "john@gmail.com",
-          student_phone: "+123252522",
-        },
-        {
-          student_name: "Moe",
-          student_email: "mary@example.com",
-          student_phone: "+18741271367",
-        },
-        {
-          student_name: "Dooley",
-          student_email: "july@example.com",
-          student_phone: "+18741279437",
-        },
-      ];
+    //   var system_students = [
+    //     {
+    //       student_name: "John",
+    //       student_email: "john@gmail.com",
+    //       student_phone: "+123252522",
+    //     },
+    //     {
+    //       student_name: "Moe",
+    //       student_email: "mary@example.com",
+    //       student_phone: "+18741271367",
+    //     },
+    //     {
+    //       student_name: "Dooley",
+    //       student_email: "july@example.com",
+    //       student_phone: "+18741279437",
+    //     },
+    //   ];
 
       var group_data = [
         { class_id: "CLS1", group_name: "group1" },
@@ -75,10 +76,13 @@ $("#body-row .collapse").collapse("hide");
         { class_id: "CLS3", group_name: "group3" },
       ];
       var system_users;
+      var system_students;
+      var all_students;
+        
       $(document).ready(function () {
         $("#tabDiv").show();
         $("#systemUserDetail").hide();
-        var settings = {
+        var class_settings = {
             "async": true,
             "crossDomain": true,
             "url": "http://127.0.0.1:8000/students_list/get/class",
@@ -88,40 +92,43 @@ $("#body-row .collapse").collapse("hide");
             "mimeType": "multipart/form-data",
         
         }
-        $.ajax(settings).done((response) => {
-            system_users = JSON.parse(response)
+        // var getData = function(url,place) {
+        //     $.ajax(url).done((res) => {
+    
+        //         place = res
+        //     }).fail(function (err){
+        //         alert(err)
+        //     })
+        // }
+    
+        $.get("http://127.0.0.1:8000/students_list/get/class").done((response) => {
+            system_users = response
             system_users.forEach((item,i) => {
                 $("#users-data").append(`<tr>
                                 <td>${item.class_id}</td>
                                 <td>${item.class_name}</td>
                                 <td><button onclick="editSystemUser('${i}')" class="btn btn-primary btn-edit"><i class="fa fa-pencil-square-o"></i></button>
                                   &nbsp
-                                  <button onclick="editSystemStudent('${i}')" class="btn btn-primary btn-edit"><i class="fa fa-list"></i></button></td>
+                                  <button onclick="editSystemStudent('${i}','${item.class_id}')" class="btn btn-primary btn-edit"><i class="fa fa-list"></i></button></td>
                                 </tr>`);
             })
-        }).fail(function (err) {
-            alert("ERROR")
+            $.get("http://127.0.0.1:8000/students_list/get/classenrolled").done((res) => {
+                console.log(res)
+                system_students = res 
+                
+            }).fail(function (err){
+                alert(err)
+            })
+            }).fail(function (err) {
+            alert(err)
         })
+        // $.get("http://127.0.0.1:8000/students_list/get/students").done((data) => {
+        //             console.log(data)
+        //                 all_students = data
+        //             }).fail(function (err) {
+        //                 alert(err)
+        //             })
 
-        system_students.forEach((item, i) => {
-          $("#student-data").append(`<tr>
-                        <td>${item.student_name}</td>
-                        <td>${item.student_email}</td>
-                        <td>${item.student_phone}</td>
-                        <td><button onclick="" class="btn btn-primary btn-edit"><i class="fa fa-trash"></i></button>
-                        </tr>`);
-        });
-
-        // system_users.forEach((item, i) => {
-        //   $("#users-data").append(`<tr>
-        //                 <td>${item.class_id}</td>
-        //                 <td>${item.class_name}</td>
-        //                 <td>${item.student_name}</td>
-        //                 <td><button onclick="editSystemUser('${i}')" class="btn btn-primary btn-edit"><i class="fa fa-pencil-square-o"></i></button>
-        //                   &nbsp
-        //                   <button onclick="editSystemStudent('${i}')" class="btn btn-primary btn-edit"><i class="fa fa fa-plus"> Add</i></button></td>
-        //                 </tr>`);
-        // });
 
         group_data.forEach((item, i) => {
           $("#group-data").append(`<tr>
@@ -131,6 +138,7 @@ $("#body-row .collapse").collapse("hide");
                         </tr>`);
         });
       });
+      
 
       function addUser() {
         console.log("Add User");
@@ -192,11 +200,21 @@ $("#body-row .collapse").collapse("hide");
         // $("#studentname").val(system_users[i].student_name);
       }
 
-      function editSystemStudent(i) {
+      function editSystemStudent(i,id) {
+        $("#student-data").empty()
+        $("#AddStudent").val(id) 
         $("#studentModal").modal();
-        $("#stud-class-id").val(system_users[i].class_id);
-        $("#stud-class-name").val(system_users[i].class_name);
-        // $("#stud-student-name").val(system_users[i].student_name);
+            system_students.forEach((item, i) => {
+                
+                if (id === item.class_enrolled.class_id){
+                    $("#student-data").append(`<tr>
+                    <td>${item.student.name}</td>
+                    <td>${item.student.email}</td>
+                    <td>${item.student.phone}</td>
+                    <td><button onclick="deleteStudent('${item.student.id}','${item.class_enrolled.id}')" class="btn btn-primary btn-edit"><i class="fa fa-trash"></i></button>
+                    </tr>`);
+                }
+              })
       }
 
       function goBack() {
@@ -215,8 +233,60 @@ $("#body-row .collapse").collapse("hide");
 
         $("#groupModal").modal();
       }
-
+      let option_length;
       function openStudentModal() {
         $('#studentModal').modal('toggle');
         $("#add-student-Modal").modal();
+        $('#add_classid').val( $("#AddStudent").val())
+        // option_length = $("#studentlist option").length
+        // console.log(option_length)
       }
+   
+      $("#AddStudent").click(() => {
+          student = $("#studentlist option:selected").val()
+          class_ = $("#add_classid").val()
+          $.ajax({
+            type: 'POST',
+            url: 'http://127.0.0.1:8000/students_list/get/classenrolled',
+            data: {
+              "student":student,
+              "class":class_,
+            },
+            success: () => {
+              location.reload()
+            }
+          })
+      })
+       
+      $("#studentlist").click(() => {
+          option_length = ($("#studentlist option").length)
+          
+          if ($("#studentlist option:selected").val() != "Select Student") {
+            $("#AddStudent").attr("disabled", false)
+          } else{
+            $("#AddStudent").attr("disabled", true)
+          }
+        if (option_length === 1) {
+                system_students.forEach((item,i) =>{
+                    if ($("#AddStudent").val() !== item.class_enrolled.class_id) {
+                        
+                       $("#studentlist").append($('<option>').val(item.student.name).text(item.student.name))
+                    }
+                })
+                // all_students.foreach((item,i) => {
+                //     $("#studentlist").append($('<option>').val(item.name).text(item.name))
+                // })
+
+            }
+        })
+    
+        function deleteStudent(sid,cid){
+            $.ajax({
+                type: 'DELETE',
+                url:'http://127.0.0.1:8000/students_list/get/classenrolled' + '?' + $.param({'sid': sid , 'cid': cid }),
+                success: () => {
+                    location.reload();
+                  
+                }
+              })
+        }
