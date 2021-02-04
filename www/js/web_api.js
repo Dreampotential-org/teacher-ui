dc656c0a459c39b3767cfbbe35f937e1f1718276
bhavinsen <senbhavin@gmail.com>
2021-02-04 11:28:58 +0530

    Fixed URL

--- www/js/web_api.js
+++ www/js/web_api.js
@@ -1,6 +1,6 @@
 function get_users() {
     get_user_list(function(users) {
-        for(var user of users) {
+        for (var user of users) {
             /// XXX @santosh add to users list section
             console.log(user)
         }
@@ -19,17 +19,12 @@ function list_question_counters(callback) {
         "mimeType": "multipart/form-data",
 
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         callback(JSON.parse(response))
-    }).fail(function (err) {
-      alert("ERROR")
+    }).fail(function(err) {
+        alert("ERROR")
     })
 }
-
-
-
-
-
 
 function list_inbound_calls(callback) {
 
@@ -43,10 +38,10 @@ function list_inbound_calls(callback) {
         "mimeType": "multipart/form-data",
 
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         callback(JSON.parse(response))
-    }).fail(function (err) {
-      alert("ERROR")
+    }).fail(function(err) {
+        alert("ERROR")
     })
 }
 
@@ -61,10 +56,10 @@ function list_services(callback) {
         "mimeType": "multipart/form-data",
 
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         callback(JSON.parse(response))
-    }).fail(function (err) {
-      alert("ERROR")
+    }).fail(function(err) {
+        alert("ERROR")
     })
 }
 
@@ -85,17 +80,17 @@ function get_user_list(callback) {
         "mimeType": "multipart/form-data",
 
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         callback(JSON.parse(response))
-    }).fail(function (err) {
-      alert("ERROR")
+    }).fail(function(err) {
+        alert("ERROR")
     })
 }
 
 
-function get_activity_list(phone,cb){
+function get_activity_list(phone, cb) {
     var form = new FormData();
-    form.append("phone", phone); 
+    form.append("phone", phone);
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -106,13 +101,13 @@ function get_activity_list(phone,cb){
         "data": form,
         "mimeType": "multipart/form-data",
         "headers": {
-            "Authorization": 'Token '+localStorage.getItem("user-token"),
+            "Authorization": 'Token ' + localStorage.getItem("user-token"),
         },
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         cb(JSON.parse(response))
-    }).fail(function (err) {
-      alert("ERROR")
+    }).fail(function(err) {
+        alert("ERROR")
     })
 }
 
@@ -135,73 +130,73 @@ function send_user_sms(to_number, msg, callback) {
         },
 
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         // change screen for code collecton
-        callback(response,null);
+        callback(response, null);
         console.log(response)
-    }).fail(function (err) {
-        callback(null,err);
-      //alert("ERROR")
+    }).fail(function(err) {
+        callback(null, err);
+        //alert("ERROR")
     });
 }
 
 
-function send_image_twilio(callback){
+function send_image_twilio(callback) {
     // alert("ready");
     swal({
-                title: "0%",
-                text: "File uploading please wait.",
-                icon: "info",
-                buttons: false,
-                closeOnEsc: false,
-                closeOnClickOutside: false,
-            });
+        title: "0%",
+        text: "File uploading please wait.",
+        icon: "info",
+        buttons: false,
+        closeOnEsc: false,
+        closeOnClickOutside: false,
+    });
     var formData = new FormData()
-    formData.append("file",$('input[type=file]')[0].files[0]);
+    formData.append("file", $('input[type=file]')[0].files[0]);
     console.log(formData);
-      var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": SERVER + 's3_uploader/upload',
-            "method": "POST",
-            "type": "POST",
-            "processData": false,
-            "contentType": false,
-            "mimeType": "multipart/form-data",
-            "data": formData,
-            "headers": {
-                "Authorization": localStorage.getItem("token")
-            }
-        };
-     console.log(settings);
-        $.ajax(settings).done(function (response) {
-            swal({
-                  title: "Good job!",
-                  text: "File uploaded successfully!",
-                  icon: "success",
-            });
-            response = JSON.parse(response);
-            console.log(response);
-            file_url = response['file_url']
-            alert(file_url);
-            upload_to_twilio(file_url);
-
-            // $('#output').html("<div> Uploaded to S3 Url: "+ file_url + "</div>");
-    
-            // var img = $('<img>');
-            // img.attr('src', file_url);
-            // img.appendTo('#output');
-        }).fail(function (response) {
-            swal({
-                  title: "Error!",
-                  text: "File upload failed!",
-                  icon: "warning",
-            });
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": SERVER + 's3_uploader/upload',
+        "method": "POST",
+        "type": "POST",
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": formData,
+        "headers": {
+            "Authorization": localStorage.getItem("token")
+        }
+    };
+    console.log(settings);
+    $.ajax(settings).done(function(response) {
+        swal({
+            title: "Good job!",
+            text: "File uploaded successfully!",
+            icon: "success",
         });
-    
+        response = JSON.parse(response);
+        console.log(response);
+        file_url = response['file_url']
+        alert(file_url);
+        upload_to_twilio(file_url);
+
+        // $('#output').html("<div> Uploaded to S3 Url: "+ file_url + "</div>");
+
+        // var img = $('<img>');
+        // img.attr('src', file_url);
+        // img.appendTo('#output');
+    }).fail(function(response) {
+        swal({
+            title: "Error!",
+            text: "File upload failed!",
+            icon: "warning",
+        });
+    });
+
 }
 
-function upload_to_twilio(image){
+function upload_to_twilio(image) {
     // console.log(user_phone);
     var form = new FormData();
     form.append("to_number", user_phone);
@@ -221,10 +216,10 @@ function upload_to_twilio(image){
         },
 
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         console.log(response)
-    }).fail(function (err) {
-      alert("ERROR")
+    }).fail(function(err) {
+        alert("ERROR")
     });
 }
 
@@ -241,20 +236,20 @@ function get_sms_to_number(to_number, callback) {
         "url": SERVER + "voip/api_voip/list_sms",
         "method": "POST",
     }
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         response = JSON.parse(response)
-        // XXX Santosh here is what msg look like to populate
-        // SMS chat interface.
-        for(var msg of response.messages) {
+            // XXX Santosh here is what msg look like to populate
+            // SMS chat interface.
+        for (var msg of response.messages) {
             console.log(msg);
         }
         callback(response.messages)
-    }).fail(function (err) {
+    }).fail(function(err) {
         alert("ERROR")
     })
 }
 
-function format_date (created_at) {
+function format_date(created_at) {
     var date = new Date(created_at * 1000)
     var hours = date.getHours()
     var minutes = date.getMinutes()
@@ -264,4 +259,4 @@ function format_date (created_at) {
     minutes = minutes < 10 ? '0' + minutes : minutes
     var strTime = hours + ':' + minutes + ' ' + ampm
     return date.toLocaleDateString('en-US') + ' ' + strTime
-  }
+}
