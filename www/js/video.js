0ce09fee3652c70ebb3073e7f6be430cd9ec66a8
dhaval7876 <dhp.devwork@gmail.com>
2021-04-20 10:37:44 +0530

    add video.js and change calls.html

---
+++ www/js/video.js
@@ -0,0 +1,65 @@
+function handleVideoUpload() {
+    var file = $('#myFile').prop('files');
+    console.log("🚀 ~ file: index.html ~ line 33 ~ handleVideoUpload ~ file", file[0])
+    GLOBAL_FILE = file[0];
+    var form = new FormData();
+    form.append('file', GLOBAL_FILE);
+
+    var settings = {
+      async: true,
+                 "crossDomain": true,
+      url: SERVER + '/s3_uploader/upload',
+      method: 'POST',
+      type: 'POST',
+      processData: false,
+      contentType: false,
+      enctype: 'multipart/form-data',
+      data: form,
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    }
+
+    console.log(settings);
+    $.ajax(settings).done(function (response) {
+      console.log("🚀 ~ file: index.html ~ line 57 ~ response", response)
+      if (response.message == "No file provided!") {
+        swal({
+          title: 'File Not Select',
+          text: response.message,
+          icon: "warning",
+          timer: 200,
+        });
+      } else {
+        swal({
+          title: 'Good job!',
+          text: 'Video uploaded successfully!',
+          icon: 'success',
+          timer: 200,
+        });
+        const file_url = response.file_url;
+        displayVideo(file_url);
+        function displayVideo(file_url) {
+            if (file_url) {
+                var strTYPE = "video/mp4";
+                $("#theSlide").append('<video id="videoplayer" style="height:500px;width:1000px"; controls> <source src="' + file_url + '" type="' + strTYPE + '"></source></video></div>')
+            }
+          $("#videoplayer")[0].load();
+        }
+      }
+
+    //   $("#video-modal").hide();
+    //   setTimeout(() => { location.reload() }, 5000);
+
+    }).fail(function (error) {
+      console.log("🚀 ~ file: index.html ~ line 56 ~ error", error)
+      swal({
+        title: 'Error!',
+        text: 'Video upload failed!',
+        icon: 'warning',
+        timer: 200,
+      });
+
+    });
+  }
+
