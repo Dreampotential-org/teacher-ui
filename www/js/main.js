c50c15cb61c1ab15177e97757e4d925351db6b36
Aaron Rosen <aaronorosen@gmail.com>
2021-07-27 17:52:35 -0700

    set_server

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,20 @@
-var SERVER = 'https://api.dreampotential.org/';
-// var SERVER = 'http://localhost:8000/'
-var HOST = 'api.dreampotential.org';
-var WEBSOCKET_HOST = 'wss://api.dreampotential.org';
+var SERVER = '';
+var HOST = '';
+var WEBSOCKET_HOST = '';
+
+function set_server() {
+    if (window.location.origin.includes("compass")) {
+        SERVER = 'http://compasssc.com:8040/';
+        HOST = 'compasssc.com:8040';
+        WEBSOCKET_HOST = 'ws://compasssc.com:8040';
+    } else {
+        SERVER = 'https://api.dreampotential.org/';
+        HOST = 'api.dreampotential.org';
+        WEBSOCKET_HOST = 'wss://api.dreampotential.org';
+    }
+};
+set_server()
+
 
 function init() {}
 
